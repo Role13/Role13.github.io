ec7c27afa877ee827ecce0a387c700a191c90a2d
Erika R <a345618@uach.mx>
2023-05-11 20:26:56 -0600

    Se agregan los demás archivos

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let linkShow = document.getElementById('show');
+let linkHide = document.getElementById('hide');
+
+function showPassword(){
+    let password = document.getElementById('password');
+    password.type = 'text';
+
+}
+
+function hidePassword(){
+    document.getElementById('password').type = 'password'
+
+}
+
+linkShow.onclick = showPassword;
+linkHide.onclick = hidePassword;
